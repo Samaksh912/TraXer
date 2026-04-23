21a0a236a3b229c47665a2ca0c578314b8617fe9
Samaksh Goel <samakshgoel789@gmail.com>
2026-04-23 07:18:49 +0530

    linux

--- lib/todos.dart
+++ lib/todos.dart
@@ -1,10 +1,12 @@
-//todo add login and signup page
-//todo connect things to database
+//todo do a full check up of the app to see what is breaking or not working
+//todo fix settings page and connect it to db
+//todo fix ui of auth pages
 //todo add adapative ui wherever possible
 //todo add voice to transaction flow integration
 //todo add full text search
 //todo add analytics page
 //todo add history page
 //todo fix homepage ui
-//todo add settings page
 //todo add notifications
+//todo implement logo
+//todo check viability for production
